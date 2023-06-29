d0cc87792cd74c6af8481b0f192b7222f0a8e637
fakecrash1 <fakecrash@gmail.com>
2023-06-29 21:20:59 +0200

    init

---
+++ scripts.js
@@ -0,0 +1,34 @@
+// Az elemek kiválasztása
+const boardSizeSelect = document.getElementById('board-size');
+const gameModeSelect = document.getElementById('game-mode');
+const startGameButton = document.getElementById('start-game');
+const gameBoardDiv = document.getElementById('game-board');
+
+// A játéktábla generálása
+function generateBoard(size) {
+    // Először töröljük a játéktábla tartalmát
+    gameBoardDiv.innerHTML = '';
+
+    // Létrehozzuk a cellákat
+    for (let i = 0; i < size * size; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gameBoardDiv.appendChild(cell);
+    }
+
+    // Beállítjuk a játéktábla szélességét a cellák száma alapján
+    gameBoardDiv.style.width = `${size * 100}px`;
+}
+
+// A játék indítása gomb eseménykezelője
+startGameButton.addEventListener('click', () => {
+    const boardSize = parseInt(boardSizeSelect.value);
+    const gameMode = gameModeSelect.value;
+
+    // A játéktábla generálása
+    generateBoard(boardSize);
+
+    // A játékmód beállítása
+    // (Ezt majd az index.js fájlban fogjuk kezelni)
+    console.log(`A játékmód: ${gameMode}`);
+});
